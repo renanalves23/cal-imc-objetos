30df99d0ca112482c532065cc2c76fcbebbd9cff
renan <renanalves23@outlook.com>
2019-09-30 20:58:11 -0300

    chore: Commit Aplicação mais prática do código

--- index.js
+++ index.js
@@ -5,11 +5,10 @@ var John = {
   weight: 105,
   height: 1.90,
   calcImc: function() {
-    return this.weight / (this.height * this.height);
+   this.imc = this.weight / (this.height * this.height);
+    return this.imc;
   }
 };
-John.imc = John.calcImc();
-console.log('O IMC de ' + John.firstName + ' é: ' + John.imc);
 
 var Mike = {
   firstName: 'Mike',
@@ -17,21 +16,23 @@ var Mike = {
   weight: 90,
   height: 1.80,
   calcImc: function() {
-    return this.weight / (this.height * this.height);
-  }
+    this.imc = this.weight / (this.height * this.height);
+     return this.imc;
+   }
 };
-  //criando dentro do objeto o "imc", baseado no retorno da função
-Mike.imc = Mike.calcImc();
-console.log('O IMC de ' + Mike.firstName + ' é: ' + Mike.imc);
 
-// console.log(Mike.calcImc(Mike.weight));
-// console.log(John.calcImc(John.weight));
+John.calcImc();
+Mike.calcImc();
+//mostrar IMC incluido no objeto
+console.log(John, Mike);
 
 
+
+//Mostra quem tem o maior IMC
 if (Mike.imc > John.imc) {
-  console.log('O maior IMC é de ' + Mike.firstName + Mike.lastname + ' e é de ' + Mike.imc);
+  console.log('O maior IMC é de ' + Mike.firstName + " " + Mike.lastname + ' e é de ' + Mike.imc);
 }else if (Mike.imc < John.imc) {
-  console.log('O maior IMC é de ' + John.firstName + John.lastname + ' e é de ' + John.imc);
+  console.log('O maior IMC é de ' + John.firstName +" " + John.lastname + ' e é de ' + John.imc);
 }else {
   console.log('É um empate');
 }
